a1e89032b5f43147bf5cda0c3b147eb32e57e756
Chung-Yuan Hung <j406660003@gmail.com>
2017-04-26 16:45:20 +0800

    Add comments in set-net2.sh

--- set-net2.sh
+++ set-net2.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# if you launch more than 2 VMs, you need to list 
+# more taps here(tap0, tap1, tap2....)
+
 /usr/sbin/openvpn --mktun --dev tap0 --user `id -un`
 #ip tuntap add tap0 mode tap
 ifconfig tap0 promisc up
