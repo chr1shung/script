cfad08de391aa49af8a9b31b631cd9d8f03e52fe
Chung-Yuan Hung <j406660003@gmail.com>
2017-04-26 16:52:58 +0800

    Update 4 new scripts

---
+++ boot.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo echo " " | sudo nc -U $PATH_OF_YOUR_NFS_FOLDER/vm1.monitor
---
+++ ftmode.sh
@@ -0,0 +1,1 @@
+sudo echo "migrate -k tcp:192.168.11.3:4441," | sudo nc -U $PATH_OF_YOUR_NFS_FOLDER/vm1.monitor
